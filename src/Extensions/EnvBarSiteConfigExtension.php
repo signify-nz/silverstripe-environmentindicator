b5992a8a2b295627a3b5d1ccd2b0dc6cba248c57
Lani Field <lani.field@signify.co.nz>
2020-06-18 17:54:26 +1200

    feat: Add site config extension.
     - add boolean to indicate if display should be disabled
     - add tab with checkbox in settings panel to provide override value

---
+++ src/Extensions/EnvBarSiteConfigExtension.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Signify\EnvBar\Extensions;
+
+use BadMethodCallException;
+use SilverStripe\Forms\CheckboxField;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\DataExtension;
+
+/**
+ * Add override boolean to the site config settings
+ * @see SilverStripe\SiteConfig\SiteConfig 
+ * to allow CMS admin to disable automatic display of the Environment Indicator
+ * bar on the frontend pages.
+ * 
+ * @author Lani Field <lani.field@signify.co.nz>
+ * @version 1.1.0
+ * @package Signify\EnvBar\Extensions
+ */
+class EnvBarSiteConfigExtension extends DataExtension
+{
+    /**
+     * Add override column to SiteConfig db record.
+     * 
+     * @var string[]
+     */
+    private static $db = [
+        'EnvBarOverride' => 'Boolean',
+    ];
+
+    /**
+     * Add Checkbox field for override value to new Environment Indicator tab
+     * in CMS Settings.
+     * 
+     * @param FieldList $fields 
+     * @return void 
+     * @throws BadMethodCallException 
+     */
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->addFieldsToTab('Root.EnvironmentIndicator', [
+            CheckboxField::create(
+                'EnvBarOverride',
+                'Tick to turn off the environment indicator bar displayed by default'
+            ),
+        ]);
+    }
+}
